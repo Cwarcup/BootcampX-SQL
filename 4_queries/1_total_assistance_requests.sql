602d246e13432fd9c7b9ca3736adef55f1175140
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-26 09:32:53 -0700

    seeds insert into table

---
+++ 4_queries/1_total_assistance_requests.sql
@@ -0,0 +1,8 @@
+-- Get the total number of assistance_requests for a teacher.
+
+SELECT COUNT(*) as total_assistances, teachers.name
+FROM teachers
+JOIN assistance_requests
+ON assistance_requests.teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
