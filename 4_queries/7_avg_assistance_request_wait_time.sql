b7b252cc3c5becdef9d7d22c1ca6c559c40b7aac
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-26 13:23:13 -0700

    up to q8

---
+++ 4_queries/7_avg_assistance_request_wait_time.sql
@@ -0,0 +1,16 @@
+-- Calculate the average time it takes to start an assistance request.
+
+-- Return just a single column here.
+
+
+SELECT avg(started_at - created_at) as average_wait_time
+FROM assistance_requests;
+
+
+
+-- Expected Result:
+
+--  average_wait_time 
+-- -------------------
+--  00:08:46.258793
+-- (1 row)
